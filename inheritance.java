e509cf441dd4176a97e7a3b337f992e8fc5b1a0e
sabari3103 <sabari6805502@gmail.com>
2024-08-30 22:14:43 +0530

    feat: add code for inheritance

---
+++ inheritance.java
@@ -0,0 +1,27 @@
+import java.lang.Syste,;
+
+public class inheritance {
+    String name;
+    int id;
+
+    void work() {
+        System.out.println("name + " is working");
+    }
+
+class Owner extends inheritance {
+        String dept;
+
+    void manage() {
+    System.out.println(name + "is manage the " + dept + "departmemnt");
+    }
+
+    public static void main(String[] args){
+    Owner own = new Owner();
+    own.name = "sam";
+    own.id = 31;
+    own.dept= "production";
+
+    own.work();
+    own.manage();
+    }
+}
