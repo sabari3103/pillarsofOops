2d13fa7327923cf8f85ea3f7e5d56f7de6bdac95
sabari3103 <sabari6805502@gmail.com>
2024-08-30 21:54:22 +0530

    feat: code for object creaton

---
+++ Oops.java
@@ -0,0 +1,26 @@
+// this program is object creation based
+public class Oops {
+    String color;
+    String model;
+    
+
+    void drive(){
+        System.out,println("the car is driving");
+    }
+   
+    }
+    public static void main(String[] args){
+        Oops obj = new Oops();
+        obj.color = "red";
+        obj.model = "tata";
+       
+
+        obj.drive();
+       
+
+        System.out.println("car model is" +obj.model);
+        System.out.println("car color is" + obj.color);
+    }
+        
+    
+}
